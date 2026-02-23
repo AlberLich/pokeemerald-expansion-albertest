729ed20bd8ac8724c78d054ef65841e866b79715
acg00151 <61428920+acg00151@users.noreply.github.com>
2026-02-23 23:44:29 +0100

    Core Nuzlocke

--- include/strings.h
+++ include/strings.h
@@ -268,7 +268,6 @@ extern const u8 gText_Birch_WhatsYourName[];
 extern const u8 gText_Birch_SoItsPlayer[];
 extern const u8 gText_Birch_YourePlayer[];
 extern const u8 gText_Birch_AreYouReady[];
-
 extern const u8 gText_Birch_NuzlockeChallenge[];
 extern const u8 gText_Birch_NoNuzlocke[];
 extern const u8 gText_Birch_YesNuzlocke[];
